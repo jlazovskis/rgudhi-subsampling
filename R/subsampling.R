b4f1ab5099680d184efe3f631cb33f8720b24626
jlazovskis <jlazovskis@gmail.com>
2025-11-15 09:03:18 +0200

    all subsampling functions as in python docs

--- R/subsampling.R
+++ R/subsampling.R
@@ -1,26 +1,96 @@
 #' Subsampling
+
+#' @description Subsample by a greedy strategy of iteratively adding the
+#'   farthest point from the current chosen point set to the subsampling. The
+#'   iteration starts with the landmark starting point.
 #'
-#' The function [sparsify_point_set()] sparsifies a point set, given an input
-#'   minimum squared distance. The function proceeds sequentially through the
-#'   input dataset, at every element marking for deletion every later element
-#'   closer than the indiciated minimum squared distance. The output is all the
-#'   elements not marked for deletion.
+#' @author Vincent Rouvreau, Marc Glisse, Masatoshi Takenouchi
 #'
-#' @param points Either a `n x d` matrix or a length-`n` list of `d`-
-#'   dimensional vectors
-#' @param min_squared_dist A numeric value specifying the minimum squared 
-#'   distance between points
+#' @param points (Iterable[Iterable[float]]) – The input point set.
 #'
-#' @return A subset of \mathrm{points} of elements at least a squared distance
-#'   \mathrm{min_squared_dist} from each other
+#' Or
+#' 
+#' @param off_file (string) – An OFF file style name.
 #'
+#' And in both cases
+#' 
+#' @param nb_points (int) – Number of points of the subsample (the subsample
+#'   may be smaller if there are fewer than `nb_points` distinct input points).
+#'   Default: all of them.
+#' @param starting_point (int) – The iteration starts with the landmark
+#'   starting point, which is the index of the point to start with. If not set,
+#'   this index is chosen randomly.
+#' @param fast (bool) – If True (default), use an implementation that is
+#'   efficient when the doubling dimension and spread are small, but slow
+#'   otherwise. If False, use the standard quadratic algorithm.
+#'
+#' @return The subsample point set, in the order they were selected by the 
+#'   greedy strategy.
 #' @export
-#' @examplesIf reticulate::py_module_available("gudhi")
-#' X <- sphere(100, 3)
-#' Y <- sparsify_point_set(X, 0.3)
-sparsify_point_set <- function(points, min_squared_dist) {
+choose_n_farthest_points <- function(points = NULL,
+                                     off_file = "",
+                                     nb_points = 18446744073709551615,
+                                     starting_point = NULL,
+                                     fast = TRUE)) {
+  gd$subsampling$choose_n_farthest_points(
+    points = points,
+    off_file = off_file,
+    nb_points = nb_points,
+    starting_point = starting_point,
+    fast = fast
+  )
+}
+
+#' @description Subsample a point set by picking random vertices.
+#'
+#' @author Vincent Rouvreau, Marc Glisse, Masatoshi Takenouchi
+#'
+#' @param points (Iterable[Iterable[float]]) – The input point set.
+#'
+#' Or
+#' 
+#' @param off_file (string) – An OFF file style name.
+#'
+#' And in both cases
+#' 
+#' @param nb_points (int) – Number of points of the subsample.
+#'
+#' @return The subsample point set.
+#' @export
+pick_n_random_points <- function(points = NULL,
+                                 off_file = "",
+                                 nb_points = 0) {
+  gd$subsampling$pick_n_random_points(
+    points = points,
+    off_file = off_file,
+    nb_points = nb_points
+  )
+}
+
+#' @description Outputs a subset of the input points so that the squared
+#'   distance between any two points is greater than `min_squared_dist`.
+#'
+#' @author Vincent Rouvreau, Marc Glisse, Masatoshi Takenouchi
+#'
+#' @param points (Iterable[Iterable[float]]) – The input point set.
+#'
+#' Or
+#' 
+#' @param off_file (string) – An OFF file style name.
+#'
+#' And in both cases
+#' 
+#' @param min_squared_dist (float) – Minimum squared distance separating the
+#'   output points.
+#'
+#' @return The subsample point set.
+#' @export
+sparsify_point_set <- function(points = NULL,
+                               off_file = "",
+                               min_squared_dist = 0.0) {
   gd$subsampling$sparsify_point_set(
     points = points,
+    off_file = off_file,
     min_squared_dist = min_squared_dist
   )
 }
